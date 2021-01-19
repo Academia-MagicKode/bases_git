82521543784e16e5c0753d2df1ba132b00567cf9
MAX <maxitorres7@yahoo.com.ar>
2021-01-19 09:42:34 -0300

    Validador inicial de usernames

---
+++ validador.py
@@ -0,0 +1,34 @@
+import random,string
+
+
+usarios=["mati7","mati","matt"]
+
+
+def agregar_numeros(n):
+    dig=string.digits
+    return "".join(random.choice(dig) for i in range(n))
+
+
+def verifica_usuario(username):
+    orig=username
+
+    if orig not in usarios:
+        return (True,orig)
+
+    while username in usarios:
+        username= username+agregar_numeros(3)
+
+    else:
+        return (False,username)
+
+
+def ingreso_usuario(username):
+    res=verifica_usuario(username)
+    if res[0]:
+        print("Usuario Disponible!")
+    else:
+        print("Usuario No disponible, intenta con :",res[1])
+
+
+
+
